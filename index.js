12ff6701520e6569285e39cea1d8dd432a0ab641
Fulbabu <fulbabucse@gmail.com>
2022-11-08 01:01:23 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +11,33 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Great Adventure with Fahim Server Running");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7ywptfp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const dbConnect = async () => {
+  try {
+    const Services = client.db("greatAdventure").collection("services");
+    app.post("/service", async (req, res) => {
+      const service = req.body;
+      const result = await Services.insertOne(service);
+      res.send(result);
+      console.log(result);
+    });
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = Services.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+};
+dbConnect().catch((err) => console.log(err.name, err.message));
 
 app.listen(port, () => {
   console.log(`Great Adventure with Fahim Server Running on: ${port}`);
